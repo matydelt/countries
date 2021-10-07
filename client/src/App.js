a380d10b6d0f6f8d1281a53c4c700f4ceff95def
matydelt <matymanda@hotmail.com>
2021-10-07 12:15:47 -0300

    filtros terminados(faltan retoques(filtro continent))

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Landing from './components/landing/landing';
 import Home from './components/home/home';
 import { getCountries } from "./redux/actions"
+import About from './components/about/about';
 // import {}
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Route path="/" exact render={() => (<Landing />)} />
       <Route path="/home" strict render={() => (<Home getCountries={getCountries} />)} />
+      <Route path="/country/" strict render={() => (<About />)} />
     </div>
   );
 }
